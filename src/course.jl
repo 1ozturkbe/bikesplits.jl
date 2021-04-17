810ba316198db79f8642352fbff806370f8534ae
Berk Ozturk <1ozturkbe@gmail.com>
2021-04-17 08:40:34 -0400

    Breaking up drag components, reorg.

--- src/course.jl
+++ src/course.jl
@@ -6,7 +6,7 @@ FTP = 300    # Watts
 Wmax = 20000 # kJs
 mass = 75    # kgs
 
-RR = 0.05    # W/(kgm/s)
+Crr = 0.05    # W/(kgm/s)
 CdA = 0.25
 
 # Course parameters
@@ -26,14 +26,14 @@ m = Model(with_optimizer(Ipopt.Optimizer))
 
 # Course variables
 @variable(m, speed[1:n] >= 0)               # speed (m/s)
-@variable(m, xtime[1:n] >= 0)         # sector time (s)
+@variable(m, xtime[1:n] >= 0)               # sector time (s)
 @variable(m, xvel[1:n] >= 0)                # horizontal velocity (m/s)
 @variable(m, yvel[1:n])                     # vertical velocity (m/s)
 
-# Course constraints
-@constraint(m, speed .* cos.(rc.gradients ./100) .== xvel)
-@constraint(m, yvel .== xvel .* rc.gradients ./ 100)
-@constraint(m, xtime .*  xvel .== rc.lengths)
+# Drag variables
+@variable(m, rrP[1:n] >= 0) # Rolling resistance power
+@variable(m, dP[1:n] >= 0) # Drag power
+@variable(m, gP[1:n]) # Gravitational power
 
 # Physiological variables
 @variable(m, Pmax >= P[1:n] >= 0) # Power in each sector
@@ -41,19 +41,31 @@ m = Model(with_optimizer(Ipopt.Optimizer))
 @variable(m, Wrec[1:n])       # W' recovery
 @variable(m, Wmax >= W[1:n] >= 0)
 
-# Physiological constraints
-@NLconstraint(m, P[1] >= (1/2*CdA) * speed[1]^3 + RR*mass*speed[1] + g*mass*yvel[1] + 
+# Course constraints
+@constraint(m, speed .* cos.(rc.gradients ./100) .== xvel)
+@constraint(m, yvel .== xvel .* rc.gradients ./ 100)
+@constraint(m, xtime .*  xvel .== rc.lengths)
+
+# Drag constraints
+@constraint(m, rrP .== Crr*mass*xvel) # TODO check assumption
+@constraint(m, gP .== g*mass*yvel)
+@NLconstraint(m, [i=1:n], dP[i] >= (1/2*CdA) * speed[i] ^ 3)
+
+# Energy conservation constraints
+@NLconstraint(m, P[1] >= dP[1] + rrP[1] + gP[1] + 
         0.5*mass*(speed[1]^2) / xtime[1])
+for i=2:n
+    @NLconstraint(m, P[i] >= dP[i] + rrP[i] + gP[i] + 
+                0.5*mass*(speed[i]^2 - speed[i-1]^2) / xtime[i])
+end
+
+# Physiological constraints
+@constraint(m, W[1] == Wmax)
 @NLconstraint(m, Wcost[1] >= FTP*exp((P[1] - FTP)/FTP) - FTP)
 @constraint(m, Wrec[1] <= 0.5*(FTP - P[1]))
 for i=2:n
-    @NLconstraint(m, P[i] >= (1/2*CdA) * speed[i]^3 + RR*mass*speed[i] + g*mass*yvel[i] + 
-                0.5*mass*(speed[i]^2 - speed[i-1]^2) / xtime[i])
     @NLconstraint(m, Wcost[i] >= FTP*exp((P[i] - FTP)/FTP) - FTP)
     @constraint(m, Wrec[i] <= 0.5*(FTP - P[i]))
-end
-@constraint(m, W[1] == Wmax)
-for i=2:n
     @constraint(m, W[i] <= W[i-1] + xtime[i-1] * Wrec[i-1] - xtime[i-1] * Wcost[i-1])
 end
 @constraint(m, P[n] <= 1/2 * Pmax) # For conditioning of the final sprint
