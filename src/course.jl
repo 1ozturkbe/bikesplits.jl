5adf77d60b03726f1d5a2dcd3c778c68cd9731e6
Berk Ozturk <1ozturkbe@gmail.com>
2021-04-19 09:39:52 -0400

    Struct refactor for pacing_model.

--- src/course.jl
+++ src/course.jl
@@ -1,99 +1,73 @@
-using JuMP, Plots, Ipopt
+using JuMP, Plots, Ipopt, Parameters
 
-g = 9.81
-Pmax = 1000  # Watts
-FTP = 300    # Watts
-Wmax = 20000 # kJs
-mass = 75    # kgs
+include("structs.jl")
 
-rho = 1.2
-Crr = 0.05    # W/(kgm/s)
-CdA = 0.25
+include("plots.jl")
 
-# Course parameters
-mutable struct Course
-    name         # string
-    lengths      # m, as the crow flies
-    gradients    # %
+rider = Rider()
+course = Course(name = "Test",
+            lengths = 100 .* ones(100), 
+            gradients = 5 .* sin.(collect(1:100) ./ 10))
+
+# Optimal pacing model
+function pacing_model(rider::Rider, course::Course, solver = Ipopt.Optimizer)
+    g = 9.81
+    m = Model(with_optimizer(solver))
+    n = course.n
+
+    # Course variables
+    @variables(m, begin
+                    speed[1:n] >= 0             # speed (m/s)
+                    xtime[1:n] >= 0             # sector time (s)
+                    xvel[1:n] >= 0              # horizontal velocity (m/s)
+                    yvel[1:n]                   # vertical velocity (m/s)
+
+    # Drag variables
+                    rrP[1:n] >= 0               # Rolling resistance power (W)
+                    dP[1:n] >= 0                # Drag power (W)
+                    gP[1:n]                     # Gravitational power (W)
+
+    # Physiological variables
+                    rider.Pmax >= P[1:n] >= 0   # Power in each sector (W)
+                    tw[1:n] >= 0                # Recovery time constant (s) 
+                    deltaP[1:n]                 # Power above CP (W)                                           
+                    Wcost[1:n] >= 0             # W' depletion rate (J/s)
+                    Wrec[1:n]                   # W' recovery rate (J/s)
+                    rider.Wmax >= W[1:n] >= 0   # W' remaining (J)
+    end)
+
+    # Course constraints
+    @constraint(m, speed .* cos.(course.gradients ./100) .== xvel)
+    @constraint(m, yvel .== xvel .* course.gradients ./ 100)
+    @constraint(m, xtime .*  xvel .== course.lengths)
+
+    # Drag constraints
+    @constraint(m, rrP .== rider.Crr*rider.mass*xvel) # TODO check assumption
+    @constraint(m, gP .== g*rider.mass*yvel)
+    @NLconstraint(m, [i=1:n], dP[i] >= (0.5*course.rho*rider.CdA) * speed[i] ^ 3)
+
+    # Energy conservation constraints
+    @NLconstraint(m, P[1] >= dP[1] + rrP[1] + gP[1] + 
+            0.5*rider.mass*(speed[1]^2) / xtime[1])
+    @NLconstraint(m, [i=2:n], P[i] >= dP[i] + rrP[i] + gP[i] + 
+            0.5*rider.mass*(speed[i]^2 - speed[i-1]^2) / xtime[i])
+
+    # Physiological constraints
+    @constraint(m, deltaP .== P .- rider.CP)
+    @constraint(m, W[1] == rider.Wmax)
+    @constraint(m, Wcost .>= deltaP)
+    @constraint(m, P[n] <= 1/2 * rider.Pmax) # For conditioning of the final sprint
+    @NLconstraint(m, [i=1:n], tw[i] >= 546*exp(-0.01*(deltaP[i])) + 316)
+    @NLconstraint(m, [i=1:n], Wrec[i] <= rider.Wmax / tw[i])
+    @NLconstraint(m, [i=2:n], W[i] <= W[i-1] + xtime[i-1] * Wrec[i-1] - xtime[i-1] * Wcost[i-1])
+
+    # Objective: minimize total time
+    @objective(m, Min, sum(xtime))  
+    return m
 end
 
-n=100
-rc = Course("Test", 
-            50 .* ones(n), 
-           5 .* sin.(collect(1:n) ./ 10))
-
-# Initializing model
-m = Model(with_optimizer(Ipopt.Optimizer))
-
-# Course variables
-@variable(m, speed[1:n] >= 0)               # speed (m/s)
-@variable(m, xtime[1:n] >= 0)               # sector time (s)
-@variable(m, xvel[1:n] >= 0)                # horizontal velocity (m/s)
-@variable(m, yvel[1:n])                     # vertical velocity (m/s)
-
-# Drag variables
-@variable(m, rrP[1:n] >= 0) # Rolling resistance power
-@variable(m, dP[1:n] >= 0) # Drag power
-@variable(m, gP[1:n]) # Gravitational power
-
-# Physiological variables
-@variable(m, Pmax >= P[1:n] >= 0) # Power in each sector
-@variable(m, tw[1:n] >= 0)        # Recovery time constant    
-@variable(m, deltaP[1:n])         # Power above CP                                               
-@variable(m, Wcost[1:n] >= 0)     # W' cost (kJ/s)
-@variable(m, Wrec[1:n])           # W' recovery (kJ/s)
-@variable(m, Wmax >= W[1:n] >= 0) 
-
-# Course constraints
-@constraint(m, speed .* cos.(rc.gradients ./100) .== xvel)
-@constraint(m, yvel .== xvel .* rc.gradients ./ 100)
-@constraint(m, xtime .*  xvel .== rc.lengths)
-
-# Drag constraints
-@constraint(m, rrP .== Crr*mass*xvel) # TODO check assumption
-@constraint(m, gP .== g*mass*yvel)
-@NLconstraint(m, [i=1:n], dP[i] >= (0.5*rho*CdA) * speed[i] ^ 3)
-
-# Energy conservation constraints
-@NLconstraint(m, P[1] >= dP[1] + rrP[1] + gP[1] + 
-        0.5*mass*(speed[1]^2) / xtime[1])
-@NLconstraint(m, [i=2:n], P[i] >= dP[i] + rrP[i] + gP[i] + 
-        0.5*mass*(speed[i]^2 - speed[i-1]^2) / xtime[i])
-
-# Physiological constraints
-@constraint(m, deltaP .== P .- FTP)
-@constraint(m, W[1] == Wmax)
-@constraint(m, Wcost[1] >= deltaP[1])
-@NLconstraint(m, tw[1] >= 546*exp(-0.01*(deltaP[1])) + 316)
-@NLconstraint(m, Wrec[1] <= Wmax / tw[1])
-for i=2:n
-    @constraint(m, Wcost[i] >= deltaP[i])
-    @NLconstraint(m, tw[i] >= 546*exp(-0.01*(deltaP[i])) + 316)
-    @NLconstraint(m, Wrec[i] <= Wmax / tw[i])
-    @NLconstraint(m, W[i] <= W[i-1] + xtime[i-1] * Wrec[i-1] - xtime[i-1] * Wcost[i-1])
-end
-@constraint(m, P[n] <= 1/2 * Pmax) # For conditioning of the final sprint
-
-# Objective: minimize total time
-@objective(m, Min, sum(xtime))
+m = pacing_model(rider, course)
 optimize!(m)
 
 # Plotting results
-xvals = [getvalue.(xtime)[1]]
-[push!(xvals, xvals[end] + getvalue.(xtime)[i]) for i=2:n];
-p1 = plot(xvals, 3.6*getvalue.(speed), label = "Speed (kph)")
-p2 = plot(xvals, rc.gradients, label = "Gradient (%)")
-p3 = plot(xvals, getvalue.(P), label = "Power (W)")
-p4 = plot(xvals, getvalue.(W), label = "W' (kJ)")
-plot(p1, p2, p3, p4, layout = (4, 1), xlabel = "Time (s)", legend = :bottomleft)
-
-# W' model validation
-# W' consumption (exponential for now)
-# Pexp = 0:25:500
-# plot(Pexp, FTP.*exp.((Pexp .- FTP)./FTP) .- FTP)
-# W' recovery (linear for now)
-# plot(Pexp, -Pexp .+ FTP)
-
-# DCP = -300:5:300;
-# timeconst = 546 .* exp.(0.01 .* (DCP));
-# plot(DCP, timeconst)
+plot_optimal_strategy(m, course)
--- src/plots.jl
+++ src/plots.jl
@@ -18,11 +18,11 @@ function scatter_2d(m::JuMP.Model, d1::Union{Array, Symbol} = :Wcost,
     end
 end
 
-function plot_optimal_strategy(model::JuMP.Model, rc::Course)
+function plot_optimal_strategy(model::JuMP.Model, course::Course)
     xvals = [getvalue.(model[:xtime])[1]]
-    [push!(xvals, xvals[end] + getvalue.(model[:xtime])[i]) for i=2:n];
+    [push!(xvals, xvals[end] + getvalue.(model[:xtime])[i]) for i=2:length(model[:xtime])];
     p1 = plot(xvals, 3.6*getvalue.(model[:speed]), label = "Speed (kph)")
-    p2 = plot(xvals, rc.gradients, label = "Gradient (%)")
+    p2 = plot(xvals, course.gradients, label = "Gradient (%)")
     p3 = plot(xvals, getvalue.(model[:P]), label = "Power (W)")
     p4 = plot(xvals, getvalue.(model[:W]), label = "W' (kJ)")
     plot(p1, p2, p3, p4, layout = (4, 1), xlabel = "Time (s)", legend = :bottomleft)
