d2eb1e5a48ba632372070b2f7d9ba35c6940daf0
Berk Ozturk <1ozturkbe@gmail.com>
2021-04-19 08:03:37 -0400

    Recovery model from [Skiba 2012], rho fix.

--- src/course.jl
+++ src/course.jl
@@ -6,6 +6,7 @@ FTP = 300    # Watts
 Wmax = 20000 # kJs
 mass = 75    # kgs
 
+rho = 1.2
 Crr = 0.05    # W/(kgm/s)
 CdA = 0.25
 
@@ -19,7 +20,7 @@ end
 n=100
 rc = Course("Test", 
             50 .* ones(n), 
-            rand(n) .+ 5 .* sin.(collect(1:n) ./ 10))
+           5 .* sin.(collect(1:n) ./ 10))
 
 # Initializing model
 m = Model(with_optimizer(Ipopt.Optimizer))
@@ -37,9 +38,11 @@ m = Model(with_optimizer(Ipopt.Optimizer))
 
 # Physiological variables
 @variable(m, Pmax >= P[1:n] >= 0) # Power in each sector
-@variable(m, Wcost[1:n] >= 0) # W' cost
-@variable(m, Wrec[1:n])       # W' recovery
-@variable(m, Wmax >= W[1:n] >= 0)
+@variable(m, tw[1:n] >= 0)        # Recovery time constant    
+@variable(m, deltaP[1:n])         # Power above CP                                               
+@variable(m, Wcost[1:n] >= 0)     # W' cost (kJ/s)
+@variable(m, Wrec[1:n])           # W' recovery (kJ/s)
+@variable(m, Wmax >= W[1:n] >= 0) 
 
 # Course constraints
 @constraint(m, speed .* cos.(rc.gradients ./100) .== xvel)
@@ -49,24 +52,25 @@ m = Model(with_optimizer(Ipopt.Optimizer))
 # Drag constraints
 @constraint(m, rrP .== Crr*mass*xvel) # TODO check assumption
 @constraint(m, gP .== g*mass*yvel)
-@NLconstraint(m, [i=1:n], dP[i] >= (1/2*CdA) * speed[i] ^ 3)
+@NLconstraint(m, [i=1:n], dP[i] >= (0.5*rho*CdA) * speed[i] ^ 3)
 
 # Energy conservation constraints
 @NLconstraint(m, P[1] >= dP[1] + rrP[1] + gP[1] + 
         0.5*mass*(speed[1]^2) / xtime[1])
-for i=2:n
-    @NLconstraint(m, P[i] >= dP[i] + rrP[i] + gP[i] + 
-                0.5*mass*(speed[i]^2 - speed[i-1]^2) / xtime[i])
-end
+@NLconstraint(m, [i=2:n], P[i] >= dP[i] + rrP[i] + gP[i] + 
+        0.5*mass*(speed[i]^2 - speed[i-1]^2) / xtime[i])
 
 # Physiological constraints
+@constraint(m, deltaP .== P .- FTP)
 @constraint(m, W[1] == Wmax)
-@NLconstraint(m, Wcost[1] >= FTP*exp((P[1] - FTP)/FTP) - FTP)
-@constraint(m, Wrec[1] <= 0.5*(FTP - P[1]))
+@constraint(m, Wcost[1] >= deltaP[1])
+@NLconstraint(m, tw[1] >= 546*exp(-0.01*(deltaP[1])) + 316)
+@NLconstraint(m, Wrec[1] <= Wmax / tw[1])
 for i=2:n
-    @NLconstraint(m, Wcost[i] >= FTP*exp((P[i] - FTP)/FTP) - FTP)
-    @constraint(m, Wrec[i] <= 0.5*(FTP - P[i]))
-    @constraint(m, W[i] <= W[i-1] + xtime[i-1] * Wrec[i-1] - xtime[i-1] * Wcost[i-1])
+    @constraint(m, Wcost[i] >= deltaP[i])
+    @NLconstraint(m, tw[i] >= 546*exp(-0.01*(deltaP[i])) + 316)
+    @NLconstraint(m, Wrec[i] <= Wmax / tw[i])
+    @NLconstraint(m, W[i] <= W[i-1] + xtime[i-1] * Wrec[i-1] - xtime[i-1] * Wcost[i-1])
 end
 @constraint(m, P[n] <= 1/2 * Pmax) # For conditioning of the final sprint
 
@@ -89,3 +93,7 @@ plot(p1, p2, p3, p4, layout = (4, 1), xlabel = "Time (s)", legend = :bottomleft)
 # plot(Pexp, FTP.*exp.((Pexp .- FTP)./FTP) .- FTP)
 # W' recovery (linear for now)
 # plot(Pexp, -Pexp .+ FTP)
+
+# DCP = -300:5:300;
+# timeconst = 546 .* exp.(0.01 .* (DCP));
+# plot(DCP, timeconst)
