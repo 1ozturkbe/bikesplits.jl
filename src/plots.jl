ccea636689189f2fded5e76fc48b303cd1f45fde
Berk Ozturk <1ozturkbe@gmail.com>
2021-04-19 09:36:39 -0400

    plot_optimal_strategy.

--- src/plots.jl
+++ src/plots.jl
@@ -17,3 +17,13 @@ function scatter_2d(m::JuMP.Model, d1::Union{Array, Symbol} = :Wcost,
         end
     end
 end
+
+function plot_optimal_strategy(model::JuMP.Model, rc::Course)
+    xvals = [getvalue.(model[:xtime])[1]]
+    [push!(xvals, xvals[end] + getvalue.(model[:xtime])[i]) for i=2:n];
+    p1 = plot(xvals, 3.6*getvalue.(model[:speed]), label = "Speed (kph)")
+    p2 = plot(xvals, rc.gradients, label = "Gradient (%)")
+    p3 = plot(xvals, getvalue.(model[:P]), label = "Power (W)")
+    p4 = plot(xvals, getvalue.(model[:W]), label = "W' (kJ)")
+    plot(p1, p2, p3, p4, layout = (4, 1), xlabel = "Time (s)", legend = :bottomleft)
+end
